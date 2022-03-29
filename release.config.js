2cdd97c155158d30bd69ddb318949749f74d7696
shixudongleo <shixudongleo@gmail.com>
2022-03-29 08:39:35 +0000

    Update json format

--- release.config.js
+++ release.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/shixudongleo/github-action-tmp-test",
-    plugins: ['@semantic-release/commit-analyzer', 
-    '@semantic-release/release-notes-generator', 
-    '@semantic-release/github']
-}
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+    ],
+};
